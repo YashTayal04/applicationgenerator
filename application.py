9ece417c59676d4fc66d3d7e085f4023d531c2a0
YashTayal04 <47032845+YashTayal04@users.noreply.github.com>
2020-05-04 20:05:07 +0530

    Used today's date as application written date

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 from mailmerge import MailMerge
 from datetime import date
-def create(w,A,To,D,T,V) :
+def create(A,To,D,T,V) :
     template = "./application.docx"
     document = MailMerge(template)
+    w=str(date.today())
     document.merge(Application=w,Audience=A,Topic=To,Date=D,Time=T,Venue=V)
     #document.merge(Application='3rd Feb 2020',Audience='1st year undergraduate student of IT Department',Topic='Python',Date='5th Feb 2020',Time='5 pm',Venue='ATC 203,204')
-    document.write('a-output.docx')
+    document.write('a1-output.docx')
