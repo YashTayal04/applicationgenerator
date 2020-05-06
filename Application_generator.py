f350ba8674748b6f3994c4031157c4870a819d76
Yash Tayal <yashtayal341@gmail.com>
2020-05-06 18:04:34 +0530

    Solved CORS issue

---
+++ Application_generator.py
@@ -0,0 +1,19 @@
+import application
+from flask import Flask, request
+from flask_cors import CORS
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CORS(app)
+@app.route('/application_generator', methods=['POST'])
+def application_generator() :
+    req_data = request.get_json()
+    audience=req_data['audience']
+    topic=req_data['topic']
+    date=req_data["date"]
+    time=req_data["time"]
+    venue=req_data['venue']
+    application.create(audience, topic, date, time, venue)
+    return "DONE"
+
+app.run()
