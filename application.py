2faedd304e68457df4bf71a2c7efdcf68fc753fb
YashTayal04 <47032845+YashTayal04@users.noreply.github.com>
2020-05-19 19:38:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
-import pythoncom
-from docx2pdf import convert
+#import pythoncom
+#from docx2pdf import convert
 from mailmerge import MailMerge
 import datetime
-def create(audience, topic, event_date, time, venue) :
-    pythoncom.CoInitialize()
+def create(audience, topic, event_date, time, venue,email) :
+  #  pythoncom.CoInitialize()
     template = "./application.docx"
     document = MailMerge(template)
     current_date=str(datetime.date.today())
     document.merge(Application=current_date,Audience=audience,Topic=topic,Date=event_date,Time=time,Venue=venue)
     file_name=str(datetime.datetime.now()).replace(":","_") + ".docx"
     document.write(file_name)
-    convert(file_name)
+   # convert(file_name)
